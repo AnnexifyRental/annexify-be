54b5b6c9251466bcd4590e08dfc23c601b5c3794
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-03-31 07:53:58 +0530

    Initial commit. project initialization

---
+++ src/test/java/com/anuradha/centralservice/CentralServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.centralservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
